7e0da466cc7aad9d2fe1dbc363cf9ef91af37628
Weiyi <ch3n.wei.yi@gmail.com>
2022-09-02 01:49:30 +0800

    Update PathFinder.java
    Completed path validity check. Problems: find shortest path from start to goal, execute path.

--- Algorithm/src/mdp/g18/algo/PathFinder.java
+++ Algorithm/src/mdp/g18/algo/PathFinder.java
@@ -1,6 +1,7 @@
 package mdp.g18.algo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PathFinder {
@@ -33,16 +34,18 @@ public class PathFinder {
 		}
 
 		// If image is too close to an obstacle, reverse
-		for (Obstacle obstacle : obstacleObjects) {
-			// TODO
+		if (ArenaFrame.obstacles[robotCurrCoord[0]][robotCurrCoord[1]] != 0) {
+			robot.reverse();
 		}
 
 		// Get best path
-		calculatePath(nodes.get(0), nextNode);
+		Path bestPath = calculatePath(nodes.get(0), nextNode);
+
+		// TODO: how to execute path???
 
 	}
 
-	public void calculatePath(Node currNode, Node nextNode) {
+	public Path calculatePath(Node currNode, Node nextNode) {
 		int[] currCoord = currNode.getCoord();
 		int[] nextCoord = nextNode.getCoord();
 
@@ -50,16 +53,33 @@ public class PathFinder {
 
 		boolean left = true;
 		// TODO: initialise path array
+		List<Path> pathArray = new ArrayList<Path>();
 
 		while (true) {
 			int[] p1 = getP1(robot, left);
 			int[] p2 = getP2(nextCoord, imageDir, left);
+			Path path = new Path();
 
 			// Find every possible path and add it to path array
-
 			if (left) {
 				left = false;
-				// Get all paths possible starting with left turn
+				/*
+				* TODO: get all paths possible (starting with left turn?)
+				* Main problem: how to loop through all possible paths as each path has a
+				* different configuration (i.e. no. and direction of turns, whether there
+				* is a straight line in the path)
+				*/
+				double l = Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2));
+				double[] v1 = new double[] {p2[0] - p1[0], p2[1] - p1[1]};
+				double[] v2 = rotatePt(v1, Math.PI/2);
+				path.setPt1(new double[] {p1[0] + (turnRad / l) * v2[0], p1[1] + (turnRad / l) * v2[1]});
+				path.setPt2(new double[] {path.getPt1()[0] + v1[0], path.getPt1()[1] + v1[1]});
+
+				// Add path to path array if it is valid
+				if (isValidPath(currCoord, path)) {
+					pathArray.add(path);
+				}
+
 
 			} else {
 				// Get all paths possible starting with right turn
@@ -67,6 +87,22 @@ public class PathFinder {
 			}
 		}
 		// Return shortest path by distance
+		return Collections.min(pathArray);
+	}
+
+	// Rotate a set of coordinates by angle
+	private double[] rotatePt(double[] coord, double angle) {
+		double[] rotatedPt = new double[] {0, 0};
+		if (angle == Math.PI/2) {
+			rotatedPt = new double[]{coord[1], -coord[0]};
+		} else if (angle == -Math.PI/2) {
+			rotatedPt = new double[] {-coord[1], coord[0]};
+		} else if (angle == Math.PI) {
+			rotatedPt = new double[] {-coord[0], -coord[1]};
+		} else {
+			rotatedPt = new double[] {coord[0] * Math.cos(angle) - coord[1] * Math.sin(angle), coord[0] * Math.sin(angle) + coord[1] * Math.cos(angle)};
+		}
+		return rotatedPt;
 	}
 
 	private int[] getP1(Robot robot, boolean left) {
@@ -194,7 +230,7 @@ public class PathFinder {
 	}
 
 	// TODO: implement logic for valid path check
-	public boolean isValidPath(int[] robotCoord) {
+	public boolean isValidPath(int[] robotCoord, Path path) {
 		if (robotCoord == null) return false;
 
 		int x = robotCoord[0];
@@ -202,7 +238,8 @@ public class PathFinder {
 		int[][] robotPos = {
 				{x - 1, y + 1}, {x, y + 1}, {x + 1, y + 1},
 				{x - 1, y}, {x, y}, {x + 1, y},
-				{x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1}
+				{x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1},
+				{(int) path.pt1[0], (int) path.pt1[1]}, {(int) path.pt2[0], (int) path.pt2[1]}
 		};
 
 		if ((0 < x) && (x < Arena.ARENA_WIDTH - 1) && (0 < y) && (y < Arena.ARENA_HEIGHT - 1)) {
@@ -214,10 +251,11 @@ public class PathFinder {
 		return true;
 	}
 
-	public class Path {
+	public class Path implements Comparable<Path>{
 		private double dist;
 		private double[] pt1;
 		private double[] pt2;
+		private String[] instructions; // Instruction format: turn at pt1, straight (if any), turn at pt2
 
 		public double getDist() {
 			return this.dist;
@@ -226,6 +264,31 @@ public class PathFinder {
 		public void setDist(double dist) {
 			this.dist = dist;
 		}
-	}
 
+		public void setPt1(double[] pt1) {
+			this.pt1 = pt1;
+		}
+
+		public double[] getPt1() {
+			return pt1;
+		}
+
+		public void setPt2(double[] pt2) {
+			this.pt2 = pt2;
+		}
+
+		public double[] getPt2() {
+			return pt2;
+		}
+
+		@Override
+		public int compareTo(Path o) {
+			if (this.getDist() > o.getDist()) {
+				return 1;
+			} else if (this.getDist() < o.getDist()) {
+				return -1;
+			}
+			return 0;
+		}
+	}
 }
