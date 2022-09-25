29dd1af3ab7ab0bc4eecfabb192073f83b11522b
Weiyi <ch3n.wei.yi@gmail.com>
2022-09-26 00:31:51 +0800

    Added obstacle list
    The obstacle list contains coordinates translated for our simulator. Coordinates came from sample arena in NTULearn.

--- Algorithm/src/mdp/g18/algo/Message.java
+++ Algorithm/src/mdp/g18/algo/Message.java
@@ -7,4 +7,13 @@ public class Message {
 	public final static String TURN_RIGHT = "D";
 	public final static String LEFT_ON_SPOT = "L000";
 	public final static String RIGHT_ON_SPOT = "R000";
+
+	/*Obstacle list with converted coordinates
+	* 1. (10, 180) image faces down
+	* 2. (60, 120) image faces up
+	* 3. (100, 70) image faces right
+	* 4. (150, 160) image faces left
+	* 5. (190, 90) image faces left
+	* 6. (130, 20) image faces right
+	* */
 }
